5e37ccb75770ef78df911e9a33d0d7b96860e11a
Ryan Scheel <scheel.ryan@gmail.com>
2014-02-12 11:16:56 -0600

    Uploading new recorded data.

---
+++ StatAnalysis/test1.c
@@ -0,0 +1,32 @@
+#include "unif01.h"
+#include "ulcg.h"
+#include "smarsa.h"
+#include <stddef.h>
+
+double GetU01(void *param, void *state)
+{
+
+}
+
+unsigned long GetBits(void *param, void *state)
+{
+
+}
+
+void Write(void *state)
+{
+
+}
+
+int main(void)
+{
+    unif01_Gen gen;
+
+    void* state; //bit string array
+    void* param; //index within the bit string that we are currently at
+
+    char *name;
+
+
+    return 0;
+}
